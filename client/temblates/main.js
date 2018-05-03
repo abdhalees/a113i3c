b0982387490418f0652fd9e251d729689ad8ac34
Abdallah AlHalees <abdallah.halees@gmail.com>
2018-05-03 14:59:39 +0300

    more styling

--- client/temblates/main.js
+++ client/temblates/main.js
@@ -17,12 +17,18 @@ var inlineTable = css`
     }
 `
 
+var w95 = css`
+  :host {
+    width: 95%;
+  }
+`
+
 module.exports = function (state, emit) {
   return html`
     <body class='flex flex-column items-center bg-near-white ${height}'>
-      <div class='${inlineTable} bg-white mt5 w-90 h-75 ba bw1 b--light-gray br2'>
-      <div class='flex bb bw1 b--light-gray w-90 mh4'>
-        <h3 class='roboto mid-gray'> URL Editor </h3>
+      <div class='${inlineTable} bg-white mt5 ${w95} h-50 ba bw1 b--light-gray br2'>
+      <div class='flex bb bw1 b--light-gray w-90 mh4 mt3'>
+        <h3 class='sans-serif mid-gray'> URL Editor </h3>
       </div>
         ${urlInput(state.url, emit)}
         ${urlParts(state.protocol, state.host, state.path, state.querystring, emit)}
