6edeb38e0033449a940a936d5fa45d27918708fe
Abdallah AlHalees <abdallah.halees@gmail.com>
2018-05-03 15:00:47 +0300

    more styling

--- client/temblates/newQuery.js
+++ client/temblates/newQuery.js
@@ -5,18 +5,18 @@ css('tachyons')
 
 var fontSize = css`
   :host {
-    font-size: 2vh;
+    font-size: 1rem;
     font-weight: 500;
   }
 `
 module.exports = function (emit) {
   return html`
           <div>
-          <label class='roboto mid-gray'> Query: </label> 
+          <label class='sans-serif mid-gray'> Query: </label>
           <li class='mv2'>
-            <input type="text" placeholder='Key:' id="newKey" class='mr2 pl2 w-20 h2 ba b--light-gray code'/>
-            <input type="text" placeholder='Value:' class='ba h2 w-20 pl2 b--light-gray code' id="newValue"/>
-            <button class='ml2 w4 h2 bg-near-white ba roboto ${fontSize}' onclick=${onClick}>Add Query</button>
+            <input type="text" placeholder='Key:' id="newKey" class='mr2 pl2 w-20 h2 ba b--light-gray sans-serif'/>
+            <input type="text" placeholder='Value:' class='ba h2 w-20 pl2 b--light-gray sans-serif' id="newValue"/>
+            <button class='ml2 h2 w4 bg-near-white ba sans-serif ${fontSize}' onclick=${onClick}>Add Query</button>
           </li>
 `
   function onClick (e) {
